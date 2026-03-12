2df7c4bac1bfc19079ccc6fb2b64d18aa9931352
Alex Hsu <alexshoe@gmail.com>
2026-03-12 15:02:42 -0400

    Verify `hoverData` key values in hover/click anywhere tests

--- test/jasmine/tests/hover_click_anywhere_test.js
+++ test/jasmine/tests/hover_click_anywhere_test.js
@@ -52,9 +52,14 @@ describe('hoveranywhere', function() {
 
             expect(hoverData).toBeDefined();
             expect(hoverData.points).toEqual([]);
+            expect(hoverData.xaxes.length).toBe(1);
+            expect(hoverData.yaxes.length).toBe(1);
+            expect(hoverData.xaxes[0]._id).toBe('x');
+            expect(hoverData.yaxes[0]._id).toBe('y');
             expect(hoverData.xvals.length).toBe(1);
             expect(hoverData.yvals.length).toBe(1);
-            expect(typeof hoverData.xvals[0]).toBe('number');
+            expect(hoverData.xvals[0]).toBeCloseTo(250 / 30, 2);
+            expect(hoverData.yvals[0]).toBeCloseTo(10 - 50 / 30, 2);
         })
         .then(done, done.fail);
     });
@@ -80,8 +85,29 @@ describe('hoveranywhere', function() {
             _hover(60, 210);
 
             expect(hoverData.points.length).toBe(1);
-            expect(hoverData.points[0].x).toBe(2);
-            expect(hoverData.points[0].y).toBe(3);
+            var pt = hoverData.points[0];
+            expect(pt.x).toBe(2);
+            expect(pt.y).toBe(3);
+            expect(pt.curveNumber).toBe(0);
+            expect(pt.pointNumber).toBe(1);
+            // xPixel/yPixel: plot-area px + margin (60+50=110, 210+50=260)
+            expect(pt.xPixel).toBeCloseTo(110, 1);
+            expect(pt.yPixel).toBeCloseTo(260, 1);
+            // bbox is page-relative (xPixel/yPixel + graph div page offset);
+            // center of bbox should equal xPixel/yPixel + page offset
+            var gLeft = gd.offsetLeft + gd.clientLeft;
+            var gTop = gd.offsetTop + gd.clientTop;
+            expect(pt.bbox).toBeDefined();
+            expect((pt.bbox.x0 + pt.bbox.x1) / 2).toBeCloseTo(110 + gLeft, 1);
+            expect((pt.bbox.y0 + pt.bbox.y1) / 2).toBeCloseTo(260 + gTop, 1);
+            expect(pt.bbox.x0).toBeLessThan(pt.bbox.x1);
+            expect(pt.bbox.y0).toBeLessThan(pt.bbox.y1);
+            expect(hoverData.xaxes.length).toBe(1);
+            expect(hoverData.yaxes.length).toBe(1);
+            expect(hoverData.xvals.length).toBe(1);
+            expect(hoverData.yvals.length).toBe(1);
+            expect(hoverData.xvals[0]).toBeCloseTo(2, 2);
+            expect(hoverData.yvals[0]).toBeCloseTo(3, 2);
         })
         .then(done, done.fail);
     });
@@ -112,11 +138,20 @@ describe('clickanywhere', function() {
         makePlot(gd, {clickanywhere: true}).then(function() {
             gd.on('plotly_click', function(d) { clickData = d; });
 
+            var bb = gd.getBoundingClientRect();
             var s = gd._fullLayout._size;
-            click(s.l + 250, s.t + 50);
+            click(bb.left + s.l + 250, bb.top + s.t + 50);
 
             expect(clickData).toBeDefined();
             expect(clickData.points).toEqual([]);
+            expect(clickData.xaxes.length).toBe(1);
+            expect(clickData.yaxes.length).toBe(1);
+            expect(clickData.xvals.length).toBe(1);
+            expect(clickData.yvals.length).toBe(1);
+            // click at 250px into 300px plot area, xrange [0,10]: 250/300*10 = 8.33
+            expect(clickData.xvals[0]).toBeCloseTo(250 / 30, 2);
+            // click at 50px into 300px plot area, yrange [0,10]: 10 - 50/300*10 = 8.33
+            expect(clickData.yvals[0]).toBeCloseTo(10 - 50 / 30, 2);
         })
         .then(done, done.fail);
     });
@@ -127,8 +162,9 @@ describe('clickanywhere', function() {
         makePlot(gd).then(function() {
             gd.on('plotly_click', function(d) { clickData = d; });
 
+            var bb = gd.getBoundingClientRect();
             var s = gd._fullLayout._size;
-            click(s.l + 250, s.t + 50);
+            click(bb.left + s.l + 250, bb.top + s.t + 50);
 
             expect(clickData).toBeUndefined();
         })
